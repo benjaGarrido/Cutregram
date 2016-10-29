2bf84dc582cc2280748ff5950b94763ad6bafbe0
Benjamín Garrido Barreiro <benjamin.garrido@gmail.com>
2016-10-29 13:24:08 +0200

    bgarrido - añadimos un provider para organizar nuestro proyecto de una forma más coherente

--- scripts/app.js
+++ scripts/app.js
@@ -5,27 +5,20 @@ var app = angular.module('Cutregram',['ngRoute']);
 
 // Inyectamos en fase config los proveedores necesarios
 
-app.config(['$httpProvider','Properties',function ($httpProvider,Properties) {
-    // Configuramos el servicio $http para que envie la cabcera necesaria
-    $httpProvider.defaults.headers.common = {
-        'X-Cutregram-Api-Key':Properties.apiKey
-    };
+app.config(['BackEndProvider','Properties',function (BackEndProvider,Properties) {
 
-    // Configuramos las cabeceras por defecto para evitar problemas de CORS.
-    $httpProvider.defaults.headers.post = {};
-    $httpProvider.defaults.headers.put = {};
-    $httpProvider.defaults.headers.patch = {};
+    BackEndProvider.establecerApiKey(Properties.apiKey);
+    BackEndProvider.habilitarPeticionesCors();
+    BackEndProvider.establecerUrlBackend(Properties.backendUrl);
 }]);
-app.config(['$routeProvider','Properties',function ($routeProvider,Properties) {
+app.config(['$routeProvider',function ($routeProvider) {
     // Definimos las distintas rutas que manejamos en la aplicación
     $routeProvider.when('/todosposts',{
         controller:'postColletionCtrl',
         templateUrl:'views/PostCollection.html',
         resolve:{
-            PostCollection:['$http',function ($http) {
-                return $http.get(Properties.backendUrl + '/posts',{
-                    'cache':true
-                });
+            PostCollection:['BackEnd',function (BackEnd) {
+                return BackEnd.obtenerTodosPosts();
             }]
         }
     });
@@ -34,9 +27,7 @@ app.config(['$routeProvider','Properties',function ($routeProvider,Properties) {
         templateUrl:'views/MyPostCollection.html',
         resolve:{
             MyPost:['$http',function ($http) {
-                return $http.get(Properties.backendUrl + '/posts/me',{
-                    'cache':true
-                });
+                return BackEnd.obtenerMisPosts();
             }]
         }
     });
---
+++ scripts/providers/Backend.js
@@ -0,0 +1,38 @@
+/**
+ * Created by benjamingarridobarreiro on 29/10/16.
+ */
+angular.module('Cutregram').provider('BackEnd',['$httpProvider',function ($httpProvider) {
+    var urlBackend ='';
+    return{
+        'establecerApiKey':function (valor) {
+            // Configuramos el servicio $http para que envie la cabcera necesaria
+            $httpProvider.defaults.headers.common = {
+                'X-Cutregram-Api-Key':valor
+            };
+        },
+        'habilitarPeticionesCors':function () {
+            // Configuramos las cabeceras por defecto para evitar problemas de CORS.
+            $httpProvider.defaults.headers.post = {};
+            $httpProvider.defaults.headers.put = {};
+            $httpProvider.defaults.headers.patch = {};
+        },
+        'establecerUrlBackend':function (valor) {
+            urlBackend=valor;
+        },
+        //Definimos el factory que se inyectará en fase de run
+        $get:['$http',function ($http) {
+            return {
+                'obtenerTodosPosts': function () {
+                    return $http.get(urlBackend + '/posts',{
+                        'cache':true
+                    });
+                },
+                'obtenerMisPosts':function () {
+                    return $http.get(urlBackend + '/posts/me',{
+                        'cache':true
+                    });
+                }
+            };
+        }]
+    };
+}]);
