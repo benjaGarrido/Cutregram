929d85cfc7a58d81e60fe2d9490e279188ce5ee0
Benjamín Garrido Barreiro <benjamin.garrido@gmail.com>
2016-11-01 11:32:32 +0100

    bgarrido - añadimos la paginacion

--- scripts/controllers/PostCollection.js
+++ scripts/controllers/PostCollection.js
@@ -3,7 +3,20 @@
  */
 angular.module('Cutregram').controller('postColletionCtrl',['$scope','PostCollection','$location',function ($scope,PostCollection,$location) {
     // Autenticarse primero cutregram-sp.appspot.com/webapp/index.html
-    $scope.posts = PostCollection.data;
+    var posts = PostCollection.data;
+
+    $scope.paginator = {
+        'pageChanged' : function () {
+            var fistElement = ($scope.paginator.actualPage - 1) * $scope.paginator.elementsPerPage;
+            var lastElement = fistElement + $scope.paginator.elementsPerPage;
+            $scope.posts = posts.slice(fistElement,lastElement);
+        },
+        'actualPage' : 1,
+        'totalElements' : posts.length,
+        'elementsPerPage' : 10
+    };
+    $scope.paginator.pageChanged();
+
     $scope.navegate = function(idPost){
         $location.path('/detallepost/' + idPost);
     };
